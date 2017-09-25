d5652924bd947af987b54a0961033862d7bcbd15
krasnozhenovk <krasnozhenovk@gmail.com>
2017-09-25 22:29:19 +0300

    fixed controller

--- public/js/addItemCtrl.js
+++ public/js/addItemCtrl.js
@@ -1,21 +1,23 @@
 dApp.controller('addItemCtrl', ['$scope', function($scope) {
 
         $scope.items = [];
-        $scope.Comments = [];
+        $scope.comments = [];
 
         $scope.addItem = function() {
             $scope.items.push({'title': $scope.newItem})
-            $scope.newItem = ''
+            $scope.newItem = '';
+            console.log('added item');
         }
 
         $scope.deleteItem = function(index) {	
             $scope.items.splice(index, 1);
         }
-        $scope.addComment = function(){
-            $scope.Comments.push({'Comment': $scope.newComment})
-            $scope.newComment = ''
+        $scope.addComment = function(e){
+            $scope.comments.push({'comment': $scope.newComment})
+            $scope.newComment = '';
+            console.log('krasava');
         }
-    }])
+}]);
 
 
 
